c12bba01c70d50655f9a88e7227bb0cf8f7c08c0
francesco <francifrison.ff@gmail.com>
2024-05-31 16:43:03 +0200

    s

--- PROGETTO/src/menu.s
+++ PROGETTO/src/menu.s
@@ -1,27 +1,11 @@
-# MENU SCELTA ALGORITMO 
-
+# STAMPA MENU SCELTA ALGORITMO (e acquisizione da tastiera)
 .section .data
 
-richiesta:
-    .ascii "Select algorithm (0 = exit, 1 = EDF, 2 = HPF) -> "
-richiesta_len:
-    .long . - richiesta
+richiesta: .ascii "Select algorithm (0 = exit, 1 = EDF, 2 = HPF) -> "
+richiesta_len: .long . - richiesta
 
-print_exit:
-    .ascii "You selected exit\n"
-print_exit_len:
-    .long . - print_exit
-
-print_alg1:
-    .ascii "Pianificazione EDF:\n"
-print_alg1_len:
-    .long . - print_alg1
-
-print_alg2:
-    .ascii "Pianificazione HPF:\n"
-print_alg2_len:
-    .long . - print_alg2
-    
+print_exit: .ascii "You selected exit\n"
+print_exit_len: .long . - print_exit
 
 .section .text
 .global menu
@@ -29,53 +13,32 @@ print_alg2_len:
 .type menu, @function
 
 menu:
-    movl $4, %eax                   # chiamo la WRITE per scrivere "Select algorithm" contenuto nell'etichetta richiesta
-    movl $1, %ebx                   # esco dalla syscall
+    movl $4, %eax                   
+    movl $1, %ebx                   
     leal richiesta, %ecx      
     movl richiesta_len, %edx
     int $0x80
 
-inserimento:
-    #scanf
-    
+inserimento: 
     call scanfd                     # il valore letto va in EAX
     cmpl $0, %eax
     je stampa_zero
     cmpl $1, %eax
-    je stampa_uno
+    je exit
     cmpl $2, %eax
-    je stampa_due
+    je exit
 
     jmp menu
 
 stampa_zero:
     pushl %eax
-    movl $4, %eax                   # chiamo la WRITE per scrivere "You selected EDF algorithm" contenuto nell'etichetta richiesta
-    movl $1, %ebx                   # esco dalla syscall
+    movl $4, %eax                   
+    movl $1, %ebx                   
     leal print_exit, %ecx      
     movl print_exit_len, %edx
     int $0x80
-    jmp exit
-
-stampa_uno:
-    pushl %eax
-    movl $4, %eax                   # chiamo la WRITE per scrivere "You selected EDF algorithm" contenuto nell'etichetta richiesta
-    movl $1, %ebx                   # esco dalla syscall
-    leal print_alg1, %ecx      
-    movl print_alg1_len, %edx
-    int $0x80
-    jmp exit
-
-stampa_due:
-    pushl %eax
-    movl $4, %eax                   # chiamo la WRITE per scrivere "You selected HPF algorithm" contenuto nell'etichetta richiesta
-    movl $1, %ebx                   # esco dalla syscall
-    leal print_alg2, %ecx      
-    movl print_alg2_len, %edx
-    int $0x80
+    popl %eax
  
 exit:
-    popl %eax
-    
     ret
 
